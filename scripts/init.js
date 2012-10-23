1a6aeb1a487bbb4a8ce014ff6e7cb8c73066888a
Justin <justin@Justin-Laptop.(none)>
2012-10-22 21:19:57 -0500

    Initial boilerplate

--- scripts/init.js
+++ scripts/init.js
@@ -1,17 +1,29 @@
 /* 
 	APP CONFIG & INIT
 */
-
+console.log('init');
 // Config
 requirejs.config({
     paths: {
         jquery: 'lib/jquery',
         _: 'lib/underscore',
         backbone: 'lib/backbone'
+    },
+    shim: {
+    	backbone: {
+            deps: ['_', 'jquery'],
+            exports: 'Backbone'
+        },
+        _: {
+            deps: ['jquery'],
+            exports: '_'
+        },
     }
 });
 
 // Start app
-requirejs(['jquery', '_', 'backbone'], function   ($, _, backbone) {
-
+requirejs(['jquery', '_', 'backbone'], function   ($, _, Backbone) {
+	console.log($);
+	console.log(_);
+	console.log(Backbone);
 });
