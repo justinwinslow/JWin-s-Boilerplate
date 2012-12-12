fc58ed2ef2a0faccc4bc2c5f55e1f182dbaf64ed
Justin <justin@Justin-Laptop.(none)>
2012-12-12 13:18:37 -0600

    renamed app folder and added jam

--- app/init.js
+++ app/init.js
@@ -13,7 +13,7 @@ requirejs.config({
         // Libraries
         jquery: 'lib/jquery',
         //underscore: 'lib/underscore', I left this as a comment so you can see how aliasing paths can make "upgrades" easier
-        underscore: 'lib/lodash',
+        underscore: 'lib/underscore/underscore',
         backbone: 'lib/backbone',
         handlebars: 'lib/handlebars',
         modernizr: 'lib/modernizr',
