dfb9f4588fb3e26900ae5f3f042fbd4c2abe654b
Justin W <justin.winslow@drakerenergy.com>
2013-02-19 16:21:00 -0600

    Updated file server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express'),
 	app = express();
 
-app.get('*', function(req, res){
-	var url = (req.url === '/')?'index.html':req.url.replace('/', '');
-
-	res.status(200).sendfile(url);
+app.get('/', function(req, res){
+	res.status(200).sendfile('index.html');
 });
+
+app.use(express.static( __dirname ));
 
 app.listen(5000);
 
