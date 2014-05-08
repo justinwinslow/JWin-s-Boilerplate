7d6113c50cfbdd06487a69eb949d80c850e92a9f
Justin Winslow <justin@jwin.me>
2014-05-07 23:54:31 -0500

    Clean up and updates

--- app/init.js
+++ app/init.js
@@ -1,5 +1,5 @@
 /*
-    APP CONFIG & INIT
+  APP CONFIG & INIT
 */
 
 // Set debug mode [think about modifying this to be dynamic]
@@ -9,55 +9,55 @@ if (debug) console.log('init');
 
 // Require config
 require.config({
-    paths: {
-        // Libraries
-        jquery: 'components/jquery/jquery',
-        //underscore: 'components/underscore', I left this as a comment so you can see how aliasing paths can make "upgrades" easier
-        underscore: 'components/lodash/lodash',
-        backbone: 'components/backbone/backbone',
-        handlebars: 'components/handlebars/handlebars',
-        modernizr: 'components/modernizr/modernizr',
-        text: 'components/text/text',
-        css: 'components/css/css',
-        // Modules
-        somemodule: 'modules/module'
+  paths: {
+    // Libraries
+    jquery: 'bower_components/jquery/dist/jquery',
+    //underscore: 'bower_components/underscore', I left this as a comment so you can see how aliasing paths can make "upgrades" easier
+    underscore: 'bower_components/lodash/dist/lodash',
+    backbone: 'bower_components/backbone/backbone',
+    handlebars: 'bower_components/handlebars/handlebars',
+    modernizr: 'bower_components/modernizr/modernizr',
+    text: 'bower_components/text/text',
+    css: 'bower_components/css/css',
+    // Modules
+    somemodule: 'modules/module'
+  },
+  shim: {
+    backbone: {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
     },
-    shim: {
-        backbone: {
-            deps: ['underscore', 'jquery'],
-            exports: 'Backbone'
-        },
-        underscore: {
-            exports: '_'
-        },
-        handlebars: {
-            exports: 'Handlebars'
-        },
-        modernizr: {
-            exports: 'Modernizr'
-        }
+    underscore: {
+      exports: '_'
+    },
+    handlebars: {
+      exports: 'Handlebars'
+    },
+    modernizr: {
+      exports: 'Modernizr'
     }
+  }
 });
 
 // Start app
 require(
-    [
-        'jquery',
-        'underscore',
-        'backbone',
-        'handlebars',
-        'somemodule'
-    ],
-    function ($, _, Backbone, Handlebars, somemodule) {
-        if (debug) console.log($);
-        if (debug) console.log(_);
-        if (debug) console.log(Backbone);
-        if (debug) console.log(Handlebars);
-        if (debug) console.log(somemodule);
+  [
+    'jquery',
+    'underscore',
+    'backbone',
+    'handlebars',
+    'somemodule'
+  ],
+  function ($, _, Backbone, Handlebars, somemodule) {
+    if (debug) console.log($);
+    if (debug) console.log(_);
+    if (debug) console.log(Backbone);
+    if (debug) console.log(Handlebars);
+    if (debug) console.log(somemodule);
 
-        window.myApp = window.myApp = {};
+    window.myApp = window.myApp = {};
 
-        myApp.myModule = new somemodule.view.main({name: 'My New Module'});
-        myApp.myModule.trigger('myEvent', {event: 'myEvent'});
-    }
+    myApp.myModule = new somemodule.view.main({name: 'My New Module'});
+    myApp.myModule.trigger('myEvent', {event: 'myEvent'});
+  }
 );
